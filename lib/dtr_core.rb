7e28513b963ca67fba23a1139ced7561070a7302
Rob Durst <me@robdurst.com>
2024-07-13 15:41:23 -0600

    move graph logic to dtr_core, 0.11.0 release

--- lib/dtr_core.rb
+++ lib/dtr_core.rb
@@ -12,4 +12,11 @@ module DTRCore
   autoload :InstructionValidator, 'dtr_core/instruction_validator'
   autoload :Instruction, 'dtr_core/instruction'
   autoload :UserDefinedType, 'dtr_core/user_defined_type'
+
+  # A graph is a collection of nodes and edges representing the structure of a DTR file.
+  module Graph
+    autoload :LCPBT_Forrest, 'dtr_core/graph/lcpbt_forrest'
+    autoload :LeftChildPreferentialBinaryTree, 'dtr_core/graph/left_child_preferential_binary_tree'
+    autoload :Silviculturist, 'dtr_core/graph/silviculturist'
+  end
 end
---
+++ lib/dtr_core/graph/lcpbt_forrest.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module DTRCore
+  module Graph
+    class LCPBT_Forrest
+      attr_accessor :trees
+
+      def initialize
+        @trees = []
+      end
+
+      def add_tree(tree)
+        @trees << tree
+      end
+
+      def traverse
+        @trees.map(&:traverse)
+      end
+
+      def traverse_with_indentation
+        @trees.map(&:traverse_with_indentation)
+      end
+
+      # This represents a traversal through a unique path, covering all trees in the forest
+      # only once. This is useful for code generation.
+      def traverse_to_ids
+        id_map = {}
+        result = []
+
+        traverse
+          .map { |x| x.map(&:id) }
+          .each do |x|
+            sub_result = []
+            x.each do |y|
+              next if id_map[y]
+
+              sub_result << y
+              id_map[y] = true
+            end
+            result << sub_result
+          end
+
+        result
+      end
+
+      def code_generator_traverse(&block)
+        traverse_with_indentation
+          .map { |tree| tree.reverse.uniq.reverse.map(&block) }
+      end
+
+      def size
+        @trees.size
+      end
+
+      def all_paths_to(instruction_id)
+        @trees.map { |x| x.all_paths_to(instruction_id) }.flatten(1).compact
+      end
+    end
+  end
+end
